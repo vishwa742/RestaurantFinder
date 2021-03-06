d0826859f2d5e17fc34ce88564b3ae03df93ccd4
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-03-06 09:07:03 -0500

    updated from local repository

--- app/views.py
+++ app/views.py
@@ -7,34 +7,7 @@ import json
 #from IPython.display import HTML
 
 
-@app.route("/")
-def index():
-    return render_template("public/index.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("public/about.html")
-
-
-@app.route("/sign-up", methods=["GET", "POST"])
-def sign_up():
-
-    if request.method == "POST":
-
-        req = request.form
-
-        username = req["username"]
-        email = req["email"]
-        password = req["password"]
-        print(username, email, password)
-
-        return redirect(request.url)
-
-    return render_template("public/sign_up.html")
-
-
-@app.route("/search", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def search():
 
     if request.method == "POST":
@@ -42,7 +15,6 @@ def search():
         search_req = request.form["search"]
         searching = search_req
         result = yelp_search(searching)
-        # print(result)
         print(search_req)
         df = pd.DataFrame.from_dict(result, orient='index')
 
@@ -55,14 +27,9 @@ def search():
         return render_template('public/about.html', variable=result, reviews=reviews,
                                ratings=ratings, address=address, img_url=img_url, phone=phone)  # Dictionary
 
-        # return(json.dump(result,indent = 6))
-
-        # return render_template('public/about.html', variable=result  ) Dataframe
-
-        # return render_template('public/simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
-
-        # print(type(df))
-        # return render_template('simple.html',  tables=[result.to_html(classes='data')], titles=df.columns.values)
-        # return redirect(request.url)
-
     return render_template("public/search.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("public/about.html")
--- app/yelp.py
+++ app/yelp.py
@@ -15,29 +15,30 @@ headers = {
     'Authorization': 'Bearer %s' % key
 }
 
+
 def yelp_search(input):
     addresses = [input]
     #addresses = ['Kearny, New Jersey','Harrison, New Jersey']
 
-    offset =np.arange(0,500,50)
+    offset = np.arange(0, 500, 50)
 
-    tuples = list(product(addresses,offset))
+    tuples = list(product(addresses, offset))
     tuples[:5]
 
     listing = []
 
-    cols = ['Name','Reviews','Rating','Address','Phone','URL']
+    cols = ['Name', 'Reviews', 'Rating', 'Address', 'Phone', 'URL']
 
-    for address,step in tuples:
+    for address, step in tuples:
         search_parameters = {
-        "location":address,
-        "limit":50,
-        "term":'',
-        "radius":805,
-        "offset":step
+            "location": address,
+            "limit": 50,
+            "term": '',
+            "radius": 805,
+            "offset": step
         }
 
-        resp = r.get(url,headers=headers, params=search_parameters)
+        resp = r.get(url, headers=headers, params=search_parameters)
         raw_data = resp.json()
         for business in raw_data['businesses']:
             name = business['name']
@@ -46,14 +47,9 @@ def yelp_search(input):
             location = business['location']['display_address'][0]
             phone = business['display_phone']
             img_url = business['image_url']
-            listing.append([name,reviews,rating,location,phone,img_url])
+            listing.append([name, reviews, rating, location, phone, img_url])
         df = pd.DataFrame.from_records(listing, index='Name', columns=cols)
-        #df=df.head()
+        # df=df.head()
         dictionary = df.to_dict()
-        
 
     return(dictionary)
-  
-
-
-
